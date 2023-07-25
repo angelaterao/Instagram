0061d463861a4621dfa19446351d2d24ce998f0b
Axt3m <angela.terao@gmail.com>
2023-07-25 16:35:39 +0200

    Create registration screen

---
+++ Instagram/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  Instagram
+//
+//  Created by Angela Terao on 25/07/2023.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let darkPeach = UIColor(red: 252/255, green: 149/255, blue: 139/255, alpha: 1)
+}
+
+
--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -8,12 +8,115 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let plusPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        let image = UIImage(named: "add-photo")
+        button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+    
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.borderStyle = .roundedRect
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    
+    let usernameTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Username"
+        tf.borderStyle = .roundedRect
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.isSecureTextEntry = true
+        tf.borderStyle = .roundedRect
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    
+    let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor.darkPeach
+        button.layer.cornerRadius = 15
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Sign Up", for: .normal)
+        button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        button.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        button.layer.shadowOpacity = 0.4
+        button.layer.shadowRadius = 0.0
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(plusPhotoButton)
+        
+        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        plusPhotoButton.anchor(top: view.topAnchor, paddingTop: 150, bottom: nil, paddingBottom: 0, right: nil, paddingRight: 0, left: nil, paddingLeft: 0, height: 100, width: 100)
+        
+        
+        setupSignUpFields()
+        
     }
+    
+    fileprivate func setupSignUpFields() {
+        
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, usernameTextField, passwordTextField])
 
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 12
+        
+        view.addSubview(stackView)
+        view.addSubview(signUpButton)
+        
+        stackView.anchor(top: plusPhotoButton.bottomAnchor, paddingTop: 40, bottom: nil, paddingBottom: 0, right: view.rightAnchor, paddingRight: 70, left: view.leftAnchor, paddingLeft: 70, height: 150, width: 0)
+        
+        signUpButton.anchor(top: stackView.bottomAnchor, paddingTop: 40, bottom: nil, paddingBottom: 0, right: view.rightAnchor, paddingRight: 115, left: view.leftAnchor, paddingLeft: 115, height: 43, width: 0)
 
+    }
 }
+
+extension UIView {
+    
+    func anchor(top: NSLayoutYAxisAnchor?, paddingTop: CGFloat, bottom: NSLayoutYAxisAnchor?, paddingBottom: CGFloat, right: NSLayoutXAxisAnchor?,  paddingRight: CGFloat, left: NSLayoutXAxisAnchor?, paddingLeft: CGFloat, height: CGFloat, width: CGFloat) {
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
+        }
+        
+        if let right = right {
+            self.rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        
+    }
+    
+}
+
 
